650b3ba8b5f22db63254db86b5aae94eca8eb113
Tobias Grömer <tobias.groemer@gmail.com>
2025-05-29 20:42:02 +0200

    update log

--- esphome/components/waveshare_epaper/waveshare_epaper.cpp
+++ esphome/components/waveshare_epaper/waveshare_epaper.cpp
@@ -4992,6 +4992,7 @@ void EPaper2P9InBWR::update_full_() {
   this->set_memory_pointer_(0, 0);
   this->command(0x24);
   for (uint32_t i = 0; i < buf_half_len; i++) {
+    ESP_LOGD(TAG, "Updating byte bw %d", i);
     this->data(this->buffer_[i]);
   }
 
@@ -5002,6 +5003,7 @@ void EPaper2P9InBWR::update_full_() {
   this->set_memory_pointer_(0, 0);
   this->command(0x26);
   for (uint32_t i = buf_half_len; i < buf_len; i++) {
+    ESP_LOGD(TAG, "Updating byte red %d", i);
     this->data(~this->buffer_[i]);  // Invert red data
   }
 
